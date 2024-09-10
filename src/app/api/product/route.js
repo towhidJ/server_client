9084482c3543da8efc61040b4f956063c279449d
towhidJ <39158631+towhidJ@users.noreply.github.com>
2024-09-11 01:48:57 +0600

    as

--- src/app/api/product/route.js
+++ src/app/api/product/route.js
@@ -33,7 +33,7 @@ export async function POST(request) {
 
   try {
     const client = await clientPromise;
-    const db = client.db("survive"); // Replace with your actual DB name
+    const db = client.db("survive");
 
     const result = await db.collection("products").insertOne({
       productName,
